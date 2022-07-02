e34343d6105dc910f774fe4214e137ac9b40783d
21A91A6111 <98648214+21A91A6111@users.noreply.github.com>
2022-07-02 11:54:30 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(i%2==1 && arr[i]%2==0)         {             c=1;             break;         }     }     if(c==1)     {         printf("False");     }     if(c==0)     {         printf("True");     }     return 0; }

---
+++ Strictly_EVEN.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(i%2==1 && arr[i]%2==0)
+        {
+            c=1;
+            break;
+        }
+    }
+    if(c==1)
+    {
+        printf("False");
+    }
+    if(c==0)
+    {
+        printf("True");
+    }
+    return 0;
+}
