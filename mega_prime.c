17acd4ca7777d9695ebe85ab6f3b9ba186d9ef28
21A91A6111 <98648214+21A91A6111@users.noreply.github.com>
2022-07-02 20:23:33 +0530

    #include<stdio.h> int prime(int n) {     int i;     for(i=2;i*i<=n;i++)     {         if(n%i==0)         {             return 0;         }     }     return 1; } int main() {    int n,f=0,r,s;    scanf("%d",&n);    if(prime(n)==1 && n!=1)    {        r=n;        while(n>0)        {            s=n%10;            if(prime(s)==1 && s!=1)            {                f=1;                n=n/10;            }            else            {                f=0;                break;            }        }        if(f==1)        {            printf("Mega Prime");        }        else        {            printf("Not Mega Prime");        }    }    else    {        printf("Not Mega Prime");    } }

--- mega_prime.c
+++ mega_prime.c
@@ -1,43 +1,49 @@
 #include<stdio.h>
-int main()
+int prime(int n)
 {
-    int i,n,t,c=0,d,s=0,l=0;
-    scanf("%d",&n);
-    t=n;
-    for(i=1;i<=n;i++)
+    int i;
+    for(i=2;i*i<=n;i++)
     {
         if(n%i==0)
         {
-            c++;
+            return 0;
         }
     }
-    if(c==2)
-    {
-        while(n)
-        {
-            int k=0;
-            d=n%10;
-            n=n/10;
-            l++;
-            for(i=1;i<=d;i++)
-            {
-                if(d%i==0)
-                {
-                   k++; 
-                }
-            }
-            if(k==2)
-            {
-                s++;
-            }
-        }
-    }
-    if(s==l && c==2)
-    {
-        printf("Mega Prime");
-    }
-    else if(l!=s || c>2)
-    {
-        printf("Not Mega Prime");
-    }
+    return 1;
 }
+int main()
+{
+   int n,f=0,r,s;
+   scanf("%d",&n);
+   if(prime(n)==1 && n!=1)
+   {
+       r=n;
+       while(n>0)
+       {
+           s=n%10;
+           if(prime(s)==1 && s!=1)
+           {
+               f=1;
+               n=n/10;
+           }
+           else
+           {
+               f=0;
+               break;
+           }
+       }
+       if(f==1)
+       {
+           printf("Mega Prime");
+       }
+       else
+       {
+           printf("Not Mega Prime");
+       }
+   }
+   else
+   {
+       printf("Not Mega Prime");
+   }
+}
+
